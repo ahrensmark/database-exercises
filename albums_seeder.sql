c9d3e0e8dc9a1f13d0919f3b8584db50d9c62de0
Mark Ahrens <ahrens.mark.a@gmail.com>
2019-08-13 14:20:25 -0500

    Basic Statements exercise through select exercises section

---
+++ albums_seeder.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+INSERT INTO ablums (artist, name, release_date, sales, genre)
+VALUES ('Eagles', 'Hotel California', '1996', '31.5', 'soft rock'),
+       ('Carole King', 'Tapistry', '1971', '15.2', 'pop'),
+       ('Fleetwood Mac', 'Rumours', '1977', '27.9', 'soft rock'),
+       ('Pink Floyd', 'Sgt. Pepper''s Lonely Hearts Club Band', '1967', '24.2', 'progressive rock'),
+       ('The Beatles', 'Tapistry', '1971', '18.2', 'rock'),
+       ('Nirvana', 'Nevermind', '1991', '16.7', 'soft Grunge'),
+       ('Santana', 'Supernatural', '1999', '20.5', 'latin rock')
+;
+
---
+++ select_exercises.sql
@@ -0,0 +1,21 @@
+USE codeup_test_db;
+
+SELECT 'The name of all albums by Pink Floyd.' AS 'Info';
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT 'The genre for Nevermind';
+SELECT genre From albums WHERE name = 'Nevermind';
+
+SELECT 'Which albums were released in the 1990s';
+SELECT * FROM albums WHERE release_date = BETWEEN 1990 AND 1999;
+
+SELECT 'Which albums had less than 20 million certified sales';
+SELECT * FROM albums Where sales < 20;
+
+SELECT 'the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?';
+SELECT * FROM albums WHERE genre = 'Rock';
+
+
